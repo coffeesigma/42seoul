8d0e4bf26e65e1f18d782302566d607050cefacd
Jeongbeen Lee <jeongbel@c6r3s6.42seoul.kr>
2023-10-14 17:02:35 +0900

    commit 231014: finish libft mandatory

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jeongbel <jeongbel@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 14:40:57 by jeongbel          #+#    #+#             */
-/*   Updated: 2023/10/13 23:45:03 by jeongbel         ###   ########.fr       */
+/*   Updated: 2023/10/14 15:49:09 by jeongbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,5 +46,9 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
