adc103a0f33daca435193eafdf4e479c4e2c6e1c
Jeongbeen Lee <jeongbel@c2r5s4.42seoul.kr>
2023-11-05 19:47:37 +0900

    commit 231105

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeongbel <jeongbel@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/29 13:50:03 by jeongbel          #+#    #+#             */
+/*   Updated: 2023/11/05 19:46:55 by jeongbel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINEJ_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+
+#endif
