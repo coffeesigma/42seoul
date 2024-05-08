15914f75f263f977db549a9b1969e31ccb5ec2e1
Jeongbeen Lee <jeongbel@cx1r2s1.42seoul.kr>
2024-05-08 17:48:22 +0900

    commit

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: jeongbel <jeongbel@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 19:52:05 by jeongbel          #+#    #+#             */
-/*   Updated: 2024/05/07 09:34:32 by jeongbel         ###   ########.fr       */
+/*   Updated: 2024/05/08 17:46:50 by jeongbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,14 @@
 
 int	main(int argc, char **argv)
 {
+	t_list	*stack_a;
+	t_list	*stack_b;
+	t_list	*op_set;
+	int		sorted_arr;
+
+	stack_a = make_stack(argc, argv);
+	stack_b = 0;
+	sorted_arr = make_arr(stack_a);
+	bubble_sort(sorted_arr);
 	
 }
--- push_swap/sort_lower.c
+++ push_swap/sort_lower.c
@@ -6,10 +6,13 @@
 /*   By: jeongbel <jeongbel@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:09:10 by jeongbel          #+#    #+#             */
-/*   Updated: 2024/05/08 17:15:35 by jeongbel         ###   ########.fr       */
+/*   Updated: 2024/05/08 17:46:09 by jeongbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sort_stack(t_list *stack_a, )
+void	sort_stack_3(t_list *stack_a, t_list *stack_b)
+{
+
+}
